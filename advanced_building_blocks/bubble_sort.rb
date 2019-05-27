81c1c2ef33e11354a0f1eab1e0b8e509a4deea3d
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-05-27 14:00:46 -0600

    fixed logic

--- advanced_building_blocks/bubble_sort.rb
+++ advanced_building_blocks/bubble_sort.rb
@@ -17,4 +17,27 @@ def bubble_sort arr
     p answer.reverse
 end
 
-bubble_sort [4,3,78,2,0,2]
+#bubble_sort [4,3,78,2,0,2]
+
+def bubble_sort_by arr
+    answer = []
+    i = 1
+    
+    until arr.length == 0
+        while i <= arr.size - 1
+            if yield(arr[i-1], arr[i]) >= 1
+                #swap places
+               el = arr.slice!(i - 1)
+               arr.insert(i, el) 
+            end
+            i += 1
+        end
+     answer << arr.pop
+     i = 1
+    end
+    p answer.reverse
+end
+
+bubble_sort_by(["hallelujah", "hi", "hello", "hey", "hair"]) do |left, right|
+    left.length - right.length
+end
