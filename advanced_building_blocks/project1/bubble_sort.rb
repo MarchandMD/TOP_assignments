0b4699e26a8032e7a8ddfc68f8763b3ff0ce21bc
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-09-12 17:43:07 -0600

    double-checked and formatted

--- advanced_building_blocks/project1/bubble_sort.rb
+++ advanced_building_blocks/project1/bubble_sort.rb
@@ -1,43 +1,55 @@
-def bubble_sort arr
-    answer = []
-    i = 1
-    
-    until arr.length == 0
-        while i <= arr.length - 1
-            if arr[i - 1] > arr[i]
-                #swap places
-               el = arr.slice!(i - 1)
-               arr.insert(i, el) 
-            end
-            i += 1
-        end
-     answer << arr.pop
-     i = 1
+#bubble_sort
+
+#########
+
+def bubble_sort(arr)
+  answer = []
+  i = 1
+
+  until arr.length == 0
+    while i <= arr.length - 1
+      if arr[i - 1] > arr[i]
+        #swap places
+        el = arr.slice!(i - 1)
+        arr.insert(i, el)
+      end
+      i += 1
     end
-    p answer.reverse
+    answer << arr.pop
+    i = 1
+  end
+  p answer.reverse
 end
 
-bubble_sort [4,3,78,2,0,2]
+bubble_sort [4, 3, 78, 2, 0, 2]
+#bubble_sort [34, 2, 67, 1, 12, 99, 22]
 
-def bubble_sort_by arr
-    answer = []
-    i = 1
-    
-    until arr.length == 0
-        while i <= arr.size - 1
-            if yield(arr[i-1], arr[i]) >= 1
-                #swap places
-               el = arr.slice!(i - 1)
-               arr.insert(i, el) 
-            end
-            i += 1
-        end
-     answer << arr.pop
-     i = 1
+#bubble_sort_by
+
+###########
+
+def bubble_sort_by(arr)
+  answer = []
+  i = 1
+
+  until arr.length == 0
+    while i <= arr.size - 1
+      if yield(arr[i - 1], arr[i]) >= 1
+        #swap places
+        el = arr.slice!(i - 1)
+        arr.insert(i, el)
+      end
+      i += 1
     end
-    p answer.reverse
+    answer << arr.pop
+    i = 1
+  end
+  p answer.reverse
 end
 
 bubble_sort_by(["hallelujah", "hi", "hello", "hey", "hair"]) do |left, right|
-    left.length - right.length
+  left.length - right.length
 end
+# bubble_sort_by(["apple", "and", "a", "aristotle", "architecture"]) do |left, right|
+#     left.length - right.length
+# end
