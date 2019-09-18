d2355e7c2df5f6d5659086c4aa55aa483b6f8ae8
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-09-17 22:25:01 -0600

    deleted an empty md file, and added some tests for caesar_cipher

--- caeser_cipher/caeser_cipher.rb
+++ caeser_cipher/caeser_cipher.rb
@@ -1,22 +1,28 @@
-def caeser_cipher(str, integer) 
-    array = str.codepoints
-    array.collect do |number|
-        if number == 32 || number <= 63 || number >= 124
-            number.chr
-        elsif (96..122).include? number
-            new_lcase_number = number + integer
-            if new_lcase_number > 122
-                (new_lcase_number - 26).chr
-            else
-                new_lcase_number.chr
-            end
-        elsif number > 64 && number < 90
-            new_ucase_number = number + integer
-            if new_ucase_number >= 91 
-                (new_ucase_number - 26).chr
-            else
-                new_ucase_number.chr
-            end
-        end
-    end.join('')
+def caesar_cipher(str, integer)
+  array = str.codepoints
+  array.collect do |number|
+    if number == 32 || number <= 63 || number >= 124
+      number.chr
+    elsif (96..122).include? number
+      new_lcase_number = number + integer
+      if new_lcase_number > 122
+        (new_lcase_number - 26).chr
+      else
+        new_lcase_number.chr
+      end
+    elsif number > 64 && number < 90
+      new_ucase_number = number + integer
+      if new_ucase_number >= 91
+        (new_ucase_number - 26).chr
+      else
+        new_ucase_number.chr
+      end
+    end
+  end.join("")
 end
+
+#p caesar_cipher("What a string!", 5)
+#p caesar_cipher("Holy moly things get goofy!", 6)
+#p caesar_cipher("Keep studying! You're doing great!", 1)
+#p caesar_cipher("If I only had a brain...", 23)
+#p caesar_cipher("U wot mate?", 2)
