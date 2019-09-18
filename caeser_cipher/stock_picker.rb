a78b78db855d7c36e15005028144b02bcc436e66
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-09-18 12:33:22 -0600

    added calls to stock_picker to test

--- caeser_cipher/stock_picker.rb
+++ caeser_cipher/stock_picker.rb
@@ -21,4 +21,8 @@ def stock_picker arr
     [arr.find_index(best_prices[0]), arr.find_index(best_prices[1])]
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
+# p stock_picker([17,3,6,9,15,8,6,1,10])
+# p stock_picker([4,6,9,34,28,12,2,16,8,44])
+# p stock_picker([20,5, 8, 4, 18, 25, 1, 6,4,500])
+# p stock_picker([6,10,12,13,4,5,3,4,29,100,4,5,9])
+# p stock_picker([6,4,5,4,4,5,3,4,20,30,4,5,9])
