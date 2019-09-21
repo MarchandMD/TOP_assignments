3720d4b49cacf03684664eaca14e8446edcd328b
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-09-20 19:25:43 -0600

    formatted with formatter

--- caeser_cipher/substrings.rb
+++ caeser_cipher/substrings.rb
@@ -1,34 +1,34 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 
-def substrings (str,array2)
-    answer = Hash.new
+def substrings(str, array2)
+  answer = Hash.new
 
-    # if array.include?(str)
-    #     answer[str] = 1
-    #     answer
-    # end
-    array1 = str.split(/\W/)
-    array1.reject! {|x| x.to_s.empty? }
+  # if array.include?(str)
+  #     answer[str] = 1
+  #     answer
+  # end
+  array1 = str.split(/\W/)
+  array1.reject! { |x| x.to_s.empty? }
 
-    j = 0
-    while j < array1.length
-        i = 0
-        while i < array2.length
-            if array1[j].downcase.include?(array2[i])
-                if !answer[array2[i]]
-                answer[array2[i]] = 1
-                else
-                    answer[array2[i]] += 1
-                end
-            end
-            i += 1
+  j = 0
+  while j < array1.length
+    i = 0
+    while i < array2.length
+      if array1[j].downcase.include?(array2[i])
+        if !answer[array2[i]]
+          answer[array2[i]] = 1
+        else
+          answer[array2[i]] += 1
         end
-        j += 1
+      end
+      i += 1
     end
+    j += 1
+  end
 
-    answer
-     #array1
+  answer
+  #array1
 end
 
-#puts substrings("below", dictionary)
+# puts substrings("below", dictionary)
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
