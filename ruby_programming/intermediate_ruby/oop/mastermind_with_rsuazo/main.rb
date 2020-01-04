52625212a735b751fd3e3d9749606514300f93b3
Michael Marchand <michaeldavidmarchand@gmail.com>
2020-01-04 13:56:05 -0700

    started testing with RSpec

--- ruby_programming/intermediate_ruby/oop/mastermind_with_rsuazo/main.rb
+++ ruby_programming/intermediate_ruby/oop/mastermind_with_rsuazo/main.rb
@@ -1,5 +1,6 @@
-class Game
+# frozen_string_literal: true
 
+class Game
   attr_accessor :colors, :secret_code
 
   def initialize
@@ -8,23 +9,22 @@ class Game
   end
 
   def play
-   prompt_user_to_make_or_break
-   break_the_code
+    prompt_user_to_make_or_break == 'b' ? break_the_code : 'making the code'
   end
 
   def prompt_user_to_make_or_break
-    puts "Would you like to (m)ake the code or (b)reak the code?"
+    puts 'Would you like to (m)ake the code or (b)reak the code?'
     loop do
       input = gets.chomp.downcase
-      if input == "m"
-        puts "you will make the code"
+      if input == 'm'
+        puts 'you will make the code'
         # will change this later
         break
-      elsif input == "b"
-        puts "you will break the code"
+      elsif input == 'b'
+        puts 'you will break the code'
         break
       else
-        puts "invalid entry; only (m) or (b) are valid entries"
+        puts 'invalid entry; only (m) or (b) are valid entries'
       end
     end
   end
@@ -32,18 +32,18 @@ class Game
   def break_the_code
     puts "enter your guess.\n"
     puts "your options are:\n"
-    puts "(r)ed, (o)range, (y)ellow, (g)reen, (b)lue, (i)indigo, (v)iolet"
+    puts '(r)ed, (o)range, (y)ellow, (g)reen, (b)lue, (i)indigo, (v)iolet'
     loop do
       color_choices = gets.chomp.downcase
       if color_choices.length != 4
-        puts "only select 4"
+        puts 'select 4'
       else
         break
       end
     end
   end
 
-  def validate(choices, possible_colors)
+  def validate(choices, _possible_colors)
     array_of_letters = choices.split('')
     i = 0
     answers = []
@@ -51,9 +51,8 @@ class Game
       answers << colors.include?(array_of_letters[i])
       i += 1
     end
-    puts answers.all?(true) ? "valid input" : "invalid input"
+    puts answers.all?(true) ? 'valid input' : 'invalid input'
   end
-
 end
 
 instance_of_game_class = Game.new
---
+++ ruby_programming/intermediate_ruby/oop/mastermind_with_rsuazo/spec/main_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'spec_helper.rb'
+
+class Game
+
+  RSpec.describe Game do
+    context "#prompt_user_to_make_or_break" do
+      it "returns either an 'm' or a 'b'" do
+      end
+    end
+  end
+
+end
