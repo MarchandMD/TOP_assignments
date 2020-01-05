4df95f5760d43c5acc15cbc603478f78443349cd
Michael Marchand <michaeldavidmarchand@gmail.com>
2020-01-04 19:26:22 -0700

    ran rubocop

--- ruby_programming/intermediate_ruby/oop/mastermind_with_rsuazo/lib/main.rb
+++ ruby_programming/intermediate_ruby/oop/mastermind_with_rsuazo/lib/main.rb
@@ -1,29 +1,28 @@
 # frozen_string_literal: true
 
 class Game
-  attr_accessor :colors, :secret_code, :user_guess
+  attr_accessor :colors, :secret_code, :user_guess, :correct_colors
 
   def initialize
     @colors = %w[r o y g b i v]
     @secret_code = @colors.sample(4)
     @user_guess = ''
+    @correct_colors = 0
   end
 
   def play
-    prompt_user_to_make_or_break == "b" ? get_guess : not_yet
+    prompt_user_to_make_or_break == 'b' ? get_guess : not_yet
+    comparing_answer(user_guess, secret_code)
   end
 
   def prompt_user_to_make_or_break(input = nil)
-    puts "Would you like to (m)ake the code or (b)reak the code?"
+    puts 'Would you like to (m)ake the code or (b)reak the code?'
     loop do
       input ||= gets.chomp.downcase
-      if input == "m"
-        puts "i haven't done this portion yet"
-        break
-      elsif input == "b"
+      if input == 'm' || input == 'b'
         break
       else
-        puts "invalid entry; only (m) or (b) are valid entries"
+        puts 'invalid entry; only (m) or (b) are valid entries'
         input = nil
       end
     end
@@ -33,17 +32,17 @@ class Game
   def get_guess(input = nil)
     puts "enter your guess.\n"
     puts "your options are:\n"
-    puts "(r)ed, (o)range, (y)ellow, (g)reen, (b)lue, (i)indigo, (v)iolet"
+    puts '(r)ed, (o)range, (y)ellow, (g)reen, (b)lue, (i)indigo, (v)iolet'
     loop do
       input ||= gets.chomp.downcase
       if input.split('').length != 4
         puts 'select only 4'
         input = nil
       elsif input.split('').all? { |x| colors.include?(x) } == false
-        puts "select only valid colors"
+        puts 'select only valid colors'
         input = nil
       elsif input.split('').uniq.length != 4
-        puts "No duplicates; try again"
+        puts 'No duplicates; try again'
         input = nil
       else
         break
@@ -52,23 +51,14 @@ class Game
     @user_guess = input
   end
 
-
   def comparing_answer(the_guess, secret_code_arr)
     guess_as_array = the_guess.split('')
     correct_colors = 8 - (secret_code_arr + guess_as_array).uniq.length
-    if correct_colors == 4
-      puts "Nice! You guessed all the correct colors"
-    elsif correct_colors == 0
-      puts "You guessed none of the right colors"
-    else
-      puts "You guessed #{correct_colors} correct colors"
-    end
+    puts "You guessed #{correct_colors} correct colors"
     correct_colors
   end
 
   def not_yet
     puts "haven't done that yet"
   end
-
 end
-
--- ruby_programming/intermediate_ruby/oop/mastermind_with_rsuazo/spec/main_spec.rb
+++ ruby_programming/intermediate_ruby/oop/mastermind_with_rsuazo/spec/main_spec.rb
@@ -1,36 +1,37 @@
+# frozen_string_literal: true
+
 require './lib/main.rb'
 
 RSpec.describe Game do
-  context "#initialize" do
-    it "has colors" do
+  context '#initialize' do
+    it 'has colors' do
       game = Game.new
       expect(game.colors).to be_an(Array)
     end
   end
 
-  context "#try_again" do
+  context '#try_again' do
     it "outputs 'you need to try again' to standard output" do
       game = Game.new
-      expect { game.try_again }.to output.to_stdout 
+      expect { game.try_again }.to output.to_stdout
     end
   end
 
-  context "#comparing_the_answer" do
-    it "returns an integer" do
+  context '#comparing_the_answer' do
+    it 'returns an integer' do
       game = Game.new
-      the_guess = "royg"
+      the_guess = 'royg'
       the_code = %w[g y i v]
       expect(game.comparing_answer(the_guess, the_code)).to be_an(Integer)
     end
   end
 
-  context "#get_guess" do
-    it "sets the @user_guess instance variable to the input string" do
+  context '#get_guess' do
+    it 'sets the @user_guess instance variable to the input string' do
       game = Game.new
       my_guess = 'royx'
       the_colors = %w[r o y g b i v]
-      expect { game.get_guess(my_guess, the_colors) }.to raise_error(NoMethodError)   
+      expect { game.get_guess(my_guess, the_colors) }.to raise_error(NoMethodError)
     end
   end
-
 end
