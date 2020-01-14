93d3c32dd310a34c4b902ffa1ff274f26317f961
Michael Marchand <michaeldavidmarchand@gmail.com>
2020-01-13 20:18:08 -0700

    added #reverse which is recursive...not built by me

--- ruby_programming/a_bit_of_computer_science/recursive_methods/additional_articles/recursion_practice.rb
+++ ruby_programming/a_bit_of_computer_science/recursive_methods/additional_articles/recursion_practice.rb
@@ -3,3 +3,8 @@ def recursive(arr)
   return print_elements(arr.slice(0, arr.length))
 end
 #  I don't really know what this mehtod does...but it does something recursively...
+
+def reverse(str)
+  return str if str.length < 2
+  reverse(str.slice(1, str.length)) + str[0]
+end
