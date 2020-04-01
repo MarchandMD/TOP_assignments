73845251a9e9ab21aa0bbdb2f2790bc3bf029795
Michael Marchand <michaeldavidmarchand@gmail.com>
2020-03-31 23:13:08 -0600

    added notes.md and then started adding notes to main.rb

--- ruby_programming/intermediate_ruby/oop/mastermind_with_rsuazo/lib/main.rb
+++ ruby_programming/intermediate_ruby/oop/mastermind_with_rsuazo/lib/main.rb
@@ -1,17 +1,25 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 class Game
   attr_accessor :colors, :secret_code, :user_guess, :correct_colors
 
   def initialize
+    # an array
     @colors = %w[r o y g b i v]
+    # an array of 4 built with the built-in Array#sample method
     @secret_code = @colors.sample(4)
+    # an empty instance variable...to be populated later; needs to be an empty string; so I probably need to change the magic comment at the top of this to false (which I just did)
     @user_guess = ''
+    # an instance variable being used to count. Starting at 0, obviously...
     @correct_colors = 0
   end
 
+  # called with Game#play; Wonder what it really does...
   def play
-    prompt_user_to_make_or_break == 'b' ? get_guess : not_yet
+    # so this method name is a little verbose: #prompt_user_to_make_or_break; could probably be reduced to #make_or_break
+    # run the #make_or_break method right away, which either returns 'b' or it does not. Before moving on, I should probably
+    # go and take a look at #make_or_break
+    make_or_break == 'b' ? get_guess : not_yet
     comparing_answer(user_guess, secret_code)
     while correct_colors != 4
       puts secret_code
@@ -22,9 +30,13 @@ class Game
     puts right_position(user_guess, secret_code)
   end
 
-  def prompt_user_to_make_or_break(input = nil)
+  # 2. Here's the make_or_break method; it takes an optional parameter. And that's probably for testing purposes....
+  # 3. So then, I know from the previous notes that I am expecting this method to either return 'b' or not...
+  # 4. Ok, so this will either return an 'm' or a 'b'. So the logic in the #play method is a little ambiguous (a lot ambiguous becuase I had to come down to the actual method here to figure out what is going on...)
+  def make_or_break(input = nil)
     puts 'Would you like to (m)ake the code or (b)reak the code?'
     loop do
+      # so this "conditional assignment" ( that would be the ||= after the 'input' of the next line)
       input ||= gets.chomp.downcase
       if input == 'm' || input == 'b'
         break
@@ -82,4 +94,8 @@ class Game
   def not_yet
     puts "haven't done that yet"
   end
+end
+
+class Two
+  # methods will go in here
 end
